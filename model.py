7ac7998019144e7ca2cb01d914478bf854a32961
Nishad Chaoji <89744466+XXGdragonXX@users.noreply.github.com>
2025-01-22 02:17:31 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,17 +1,22 @@
 import os
 from langchain_community.llms import HuggingFaceHub
-from langchain.prompts import PromptTemplate
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory
 from langchain.chains import RetrievalQAWithSourcesChain
 
-os.environ["HUGGINGFACEHUB_API_TOKEN"] ='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'
+# Fetch the API token from the environment variables
+huggingfacehub_api_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
+# Ensure the token is set correctly before proceeding
+if not huggingfacehub_api_token:
+    raise ValueError("HUGGINGFACEHUB_API_TOKEN is not set. Please set it in the environment.")
 
-def create_llm(repo_id,retriever):
-    llm = HuggingFaceHub(repo_id = repo_id, model_kwargs={"temperature":0.5, "max_length":64})
+def create_llm(repo_id, retriever):
+    llm = HuggingFaceHub(
+        repo_id=repo_id,
+        model_kwargs={"temperature": 0.5, "max_length": 64},
+        huggingfacehub_api_token=huggingfacehub_api_token
+    )
     chains = RetrievalQAWithSourcesChain.from_llm(
-    llm = llm,
-    retriever = retriever
+        llm=llm,
+        retriever=retriever
     )
     return chains
